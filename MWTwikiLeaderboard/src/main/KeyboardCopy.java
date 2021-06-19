4dba09a155e6dfea6482a5b7babca907b5ecf955
Guy732 <81652178+Guy732@users.noreply.github.com>
2021-06-20 00:01:38 +0200

    Changed a System.out.print(s + '\n') to System.out.println(s) Yeah... it's here for a reason

--- MWTwikiLeaderboard/src/main/KeyboardCopy.java
+++ MWTwikiLeaderboard/src/main/KeyboardCopy.java
@@ -13,7 +13,7 @@ import java.awt.datatransfer.Clipboard;
 public class KeyboardCopy {
 
 	public static void Copy(String copy) {
-		System.out.print(copy + "\n");
+		System.out.println(copy);
 		StringSelection stringSelection = new StringSelection(copy);
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		clipboard.setContents(stringSelection, null);
